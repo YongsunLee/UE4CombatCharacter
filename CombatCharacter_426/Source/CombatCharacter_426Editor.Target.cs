1f6cf3717923f9b35162eed9c90be58a297f5de2
YongsunLee <katarite715@naver.com>
2024-06-14 16:38:33 +0900

    From Input System To Enhanced Input System 이전 완료

--- CombatCharacter_426/Source/CombatCharacter_426Editor.Target.cs
+++ CombatCharacter_426/Source/CombatCharacter_426Editor.Target.cs
@@ -8,7 +8,8 @@ public class CombatCharacter_426EditorTarget : TargetRules
 	public CombatCharacter_426EditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.AddRange( new string[] { "CombatCharacter_426" } );
-	}
+    }
 }
